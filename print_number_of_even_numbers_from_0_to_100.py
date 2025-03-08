93e27628835c4fd76305781959040bd1c56d2cf3
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-08 18:06:38 +0800

    variable and loop created

--- print_number_of_even_numbers_from_0_to_100.py
+++ print_number_of_even_numbers_from_0_to_100.py
@@ -1,5 +1,11 @@
 # initialize the "even_count" variable to 0
+even_count = 0
+
 # using for loop, iterate over the numbers from 0 to 100
-# check if the number is even by using the modulus operator (%)
-# if the number is even, increment the "even_count" variable by 1
+for i in range(101):
+    # check if the number is even by using the modulus operator (%)
+    if i % 2 == 0:
+    # if the number is even, increment the "even_count" variable by 1
+        even_count += 1
+
 # print the "even_count" variable after the loop finishes

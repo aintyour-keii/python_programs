47f927a44019af8e3671fab09e8af585d95648f5
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-08 21:35:11 +0800

    pseudo code

---
+++ print_all_odd_numbers_from_0_to_100.py
@@ -0,0 +1,5 @@
+# Create a variable to hold the current number
+# Initialize the current number to 0
+# Using a while loop, iterate over 0 to 100 (inclusive)
+# Identify if the current number is odd by using the modulo operator (%)
+# If the current number is odd, print the number

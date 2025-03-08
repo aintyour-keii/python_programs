a8111693d88fbaf4c948f4eb1078040166a0115f
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-08 20:15:08 +0800

    pseudo code

---
+++ print_difference_of_a_number_and_sum_of_9_numbers.py
@@ -0,0 +1,6 @@
+# Ask for the first number input
+# Create a variable to store the sum of the remaining 9 numbers
+# Using a for loop, ask for a number input 9 times
+# Add each number to the sum variable
+# Calculate the difference of the first number and the sum of the remaining 9 numbers
+# Print the result

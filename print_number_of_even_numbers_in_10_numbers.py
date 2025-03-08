6ee5dbf86320639aecf104c517123e66f1535adf
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-08 20:33:06 +0800

    pseudo code

---
+++ print_number_of_even_numbers_in_10_numbers.py
@@ -0,0 +1,4 @@
+# Create a variable for keeping track of the number of even numbers
+# Using a for loop ask for a number input 10 times
+# Check if the number input is even, if it is, add 1 to the variable
+# Print the number of even numbers after the loop

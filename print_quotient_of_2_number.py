ea5ca142efaee32c90ad225296cb7bafb7775542
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-08 17:40:44 +0800

    input added

--- print_quotient_of_2_number.py
+++ print_quotient_of_2_number.py
@@ -1,3 +1,7 @@
 # Ask for 2 float inputs
+
+num1 = float(input("Enter the first number(decimal allowed): "))
+num2 = float(input("Enter the second number(decimal allowed): "))
+
 # Divide the 2 inputs
 # Print the result

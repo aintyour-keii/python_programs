84db31bcb32b2b5228a7117e280ce7e9447d99bd
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-08 17:44:02 +0800

    dividing and printing

--- print_quotient_of_2_number.py
+++ print_quotient_of_2_number.py
@@ -3,5 +3,9 @@
 num1 = float(input("Enter the first number(decimal allowed): "))
 num2 = float(input("Enter the second number(decimal allowed): "))
 
+# Check if the 2nd input is not 0, if it is 0 then return otherwise proceed with division
 # Divide the 2 inputs
 # Print the result
+if num2 != 0:
+    result = num1/num2
+    print(result)

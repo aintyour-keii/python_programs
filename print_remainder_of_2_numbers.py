0f6c591a47d33c7500615d6b0617eed6dc7855e7
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-08 20:11:19 +0800

    pseudo code

---
+++ print_remainder_of_2_numbers.py
@@ -0,0 +1,4 @@
+# Ask for 2 number inputs
+# Check if the 2nd input is 0, if it is not proceed with calculating the remainder 
+# Calculate the remainder of the division of the two numbers using the modulus operator (%)
+# Print the remainder

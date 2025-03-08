fdc9d933b202f4478936bea0af3b9fc5fd45083b
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-08 17:27:38 +0800

    input added

--- print_equal_if_2_numbers_are_the_same.py
+++ print_equal_if_2_numbers_are_the_same.py
@@ -1,3 +1,7 @@
 # Ask for 2 inputs
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
 # Check if the inputs are the same
 # If they are the same, print "Equal"

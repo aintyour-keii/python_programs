1b68a95668c537839fe7f649b740346ebccb4b56
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-08 17:24:17 +0800

    comparing and printing

--- print_bigger_number_from_2_numbers.py
+++ print_bigger_number_from_2_numbers.py
@@ -5,3 +5,8 @@ num2 = int(input("Enter the second number: "))
 
 # Compare the 2 inputs and identify the bigger input
 # Print the bigger input
+
+if num1 > num2:
+    print(num1)
+else:
+    print(num2)

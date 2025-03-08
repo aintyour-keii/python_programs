a8b4fe848e5fb637751f8282d0306d3b7cf05866
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-08 21:44:14 +0800

    pseudo code

---
+++ print_all_numbers_from_the_first_number_to_the_second_number.py
@@ -0,0 +1,4 @@
+# Ask for 2 number inputs
+# Identify the start and end (inclusive) by their value
+# start - smaller number and end - larger number
+# Print the range of numbers from start to end (inclusive)

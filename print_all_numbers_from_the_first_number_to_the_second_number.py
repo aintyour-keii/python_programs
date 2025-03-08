5aa44348bca2fbc46540a86548f094cb30425cc6
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-08 21:44:55 +0800

    code completion

--- print_all_numbers_from_the_first_number_to_the_second_number.py
+++ print_all_numbers_from_the_first_number_to_the_second_number.py
@@ -1,4 +1,11 @@
 # Ask for 2 number inputs
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
 # Identify the start and end (inclusive) by their value
+start = min(num1, num2)
 # start - smaller number and end - larger number
+end = max(num1, num2)
 # Print the range of numbers from start to end (inclusive)
+for i in range(start, end + 1):
+    print(i)

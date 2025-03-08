77113df56f96b07416a853a1d518fd5f0677dd41
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-08 21:28:50 +0800

    code completion

--- print_number_of_even_numbers_in_10_numbers.py
+++ print_number_of_even_numbers_in_10_numbers.py
@@ -1,4 +1,11 @@
 # Create a variable for keeping track of the number of even numbers
+even_count = 0
+
 # Using a for loop ask for a number input 10 times
-# Check if the number input is even, if it is, add 1 to the variable
+for i in range(10):
+    # Check if the number input is even, if it is, add 1 to the variable
+    if int(input("Enter a number: ")) % 2 == 0:
+        even_count += 1
+
 # Print the number of even numbers after the loop
+print(even_count)

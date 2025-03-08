0465934f22ea22ff6a7c776c4d6e58f26b84321a
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-08 17:32:21 +0800

    adding and printing

--- print_sum_of_2_numbers.py
+++ print_sum_of_2_numbers.py
@@ -4,4 +4,8 @@ num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
 # Add the inputs together
+
+result = num1 + num2
+
 # Print the result
+print(result)

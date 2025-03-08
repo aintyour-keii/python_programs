bd3ca8abf9d44d753fc70e4b97d8e6598243af6a
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-08 20:17:36 +0800

    first number input and sum variable initiated

--- print_difference_of_a_number_and_sum_of_9_numbers.py
+++ print_difference_of_a_number_and_sum_of_9_numbers.py
@@ -1,5 +1,7 @@
 # Ask for the first number input
+first_number = int(input("Enter the first number: "))
 # Create a variable to store the sum of the remaining 9 numbers
+sum_remaining_numbers = 0
 # Using a for loop, ask for a number input 9 times
 # Add each number to the sum variable
 # Calculate the difference of the first number and the sum of the remaining 9 numbers

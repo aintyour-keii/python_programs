9b7deae1b91c68fa97a3946a0807d863633a07d1
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-08 17:58:56 +0800

    variable and loop created

--- print_number_of_odd_numbers_in_10_numbers.py
+++ print_number_of_odd_numbers_in_10_numbers.py
@@ -1,5 +1,11 @@
 # initialize the "odd_count" variable to 0
+odd_count = 0
 # loop through 10 times
-# in each iteration, ask for a number input
-# check if the input is odd if it is add 1 to the "odd_count" variable
+for i in range(10):
+    # in each iteration, ask for a number input
+    num = int(input("Enter a number: "))
+    # check if the input is odd if it is add 1 to the "odd_count" variable
+    if num % 2 != 0:
+        odd_count += 1
+
 # print the "odd_count" variable

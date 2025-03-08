ebccef040de172b16e1f29cb6279ba1dedbbad7d
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-08 21:41:35 +0800

    code completion

--- print_all_numbers_from_0_to_100_except_numbers_ending_in_0_or_5.py
+++ print_all_numbers_from_0_to_100_except_numbers_ending_in_0_or_5.py
@@ -1,5 +1,10 @@
 # Using a for loop, iterate to 0 - 100 (inclusive)
-# Convert the number to string
-# Get the last character of the string
-# Check if the last character of the string is not "0" or "5"
-# If the last character of the string is not "0" or "5", print the number
+for i in range(101):
+    # Convert the number to string
+    str_i = str(i)
+    # Get the last character of the string
+    last_char = str_i[-1]
+    # Check if the last character of the string is not "0" or "5"
+    if last_char not in ["0", "5"]:
+    # If the last character of the string is not "0" or "5", print the number
+        print(i)

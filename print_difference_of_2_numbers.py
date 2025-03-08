647db665363b5d5efc5299496d443fccf192bb26
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-08 20:03:49 +0800

    code completion

--- print_difference_of_2_numbers.py
+++ print_difference_of_2_numbers.py
@@ -1,3 +1,10 @@
 # Ask for 2 number inputs
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
 # Subtract the 2nd input from the 1st input
 # Print the result
+
+result = num1 - num2
+print(result)

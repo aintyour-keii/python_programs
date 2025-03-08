57b1f1426dca5260547e8657348c92b9ff2149f4
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-08 17:51:22 +0800

    pseudo code

---
+++ print_sum_of_10_numbers.py
@@ -0,0 +1,5 @@
+# Initialize total_sum variable to 0
+# Loop through 10 times
+# Ask for a number input per loop
+# Add the input to the total_sum variable
+# Print the total_sum variable after the loop finishes

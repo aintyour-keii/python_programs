9ef48690ca691973e5d22f6670b9fa72fe2f4aa3
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-08 20:08:28 +0800

    code completion

--- print_qutioent_with_no_decimal_of_2_numbers.py
+++ print_qutioent_with_no_decimal_of_2_numbers.py
@@ -1,4 +1,11 @@
 # Ask for 2 number inputs
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
 # Check if the 2nd input is not 0, if it is not, proceed with floor division
-# Using the built-in floor division, divide the 1st input by the 2nd input
-# Print the result of the division
+if num2 != 0:
+    # Using the built-in floor division, divide the 1st input by the 2nd input
+    result = num1 // num2
+    # Print the result of the division
+    print(result)

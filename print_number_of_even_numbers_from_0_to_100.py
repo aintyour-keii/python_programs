4727dcb737f48152180202ffff440066d37c9e93
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-08 18:02:52 +0800

    pseudo code

---
+++ print_number_of_even_numbers_from_0_to_100.py
@@ -0,0 +1,5 @@
+# initialize the "even_count" variable to 0
+# using for loop, iterate over the numbers from 0 to 100
+# check if the number is even by using the modulus operator (%)
+# if the number is even, increment the "even_count" variable by 1
+# print the "even_count" variable after the loop finishes

760866465ac047f9f3f1f6ac85621e289eb144cc
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-08 17:48:42 +0800

    raising and printing

--- print_number_raised_by_another_number.py
+++ print_number_raised_by_another_number.py
@@ -4,4 +4,8 @@ num1 = int(input("Enter the first number(base): "))
 num2 = int(input("Enter the second number(exponent): "))
 
 # Raise the first input by the second input using "**"
+
+result = num1**num2
+
 # Print the result
+print(result)

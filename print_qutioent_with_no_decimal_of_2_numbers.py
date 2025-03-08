693de29d9d661247fb78db95dff46c98b2167b5c
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-08 20:07:02 +0800

    pseudo code

---
+++ print_qutioent_with_no_decimal_of_2_numbers.py
@@ -0,0 +1,4 @@
+# Ask for 2 number inputs
+# Check if the 2nd input is not 0, if it is not, proceed with floor division
+# Using the built-in floor division, divide the 1st input by the 2nd input
+# Print the result of the division

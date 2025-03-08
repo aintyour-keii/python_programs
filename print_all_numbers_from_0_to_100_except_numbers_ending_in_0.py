bb84ae214396feb9ab19c443496fd94b279c42cc
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-08 18:12:49 +0800

    pseudo code

---
+++ print_all_numbers_from_0_to_100_except_numbers_ending_in_0.py
@@ -0,0 +1,5 @@
+# using a for loop, iterate from 0 - 100
+# convert the number to string
+# check the last digit of the string using string slicing
+# convert the last digit to int and check if it is 0
+# if it is 0 continue the loop otherwise print the number

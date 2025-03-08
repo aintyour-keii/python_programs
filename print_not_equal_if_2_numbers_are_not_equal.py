fb4b63820cf442249ae76f4f4df561a3b01d0766
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-08 19:16:13 +0800

    code completion

--- print_not_equal_if_2_numbers_are_not_equal.py
+++ print_not_equal_if_2_numbers_are_not_equal.py
@@ -1,3 +1,9 @@
 # Ask for 2 number inputs
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
 # Check if the numbers are not equal
 # If so print "Not Equal"
+if num1 != num2:
+    print("Not Equal")

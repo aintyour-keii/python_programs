0cdba735e2f9fe768e29b4a28325c528b1423477
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-08 17:53:06 +0800

    variable and loop created

--- print_sum_of_10_numbers.py
+++ print_sum_of_10_numbers.py
@@ -1,5 +1,10 @@
 # Initialize total_sum variable to 0
+total_sum = 0
 # Loop through 10 times
-# Ask for a number input per loop
-# Add the input to the total_sum variable
+for i in range(10):
+    # Ask for a number input per loop
+    number_input = int(input("Enter a number: "))
+    # Add the input to the total_sum variable
+    total_sum += number_input
+
 # Print the total_sum variable after the loop finishes

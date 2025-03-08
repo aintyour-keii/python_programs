c026e5691306f07baf8de6466d78d73b0a2c4426
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-08 20:12:11 +0800

    code completion

--- print_remainder_of_2_numbers.py
+++ print_remainder_of_2_numbers.py
@@ -1,4 +1,11 @@
 # Ask for 2 number inputs
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
 # Check if the 2nd input is 0, if it is not proceed with calculating the remainder 
-# Calculate the remainder of the division of the two numbers using the modulus operator (%)
-# Print the remainder
+if num2 != 0:
+    # Calculate the remainder of the division of the two numbers using the modulus operator (%)
+    remainder = num1 % num2
+    # Print the remainder
+    print(remainder)

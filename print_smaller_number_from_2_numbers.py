9124cf0ebddc5d7729ae6526cad513ae48501a9f
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-08 18:58:10 +0800

    code completion

--- print_smaller_number_from_2_numbers.py
+++ print_smaller_number_from_2_numbers.py
@@ -1,3 +1,11 @@
 # Ask for 2 number inputs
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
 # Compare the 2 inputs and identify the smaller number
 # Print the smaller number
+if num1 < num2:
+    print(num1)
+else:
+    print(num2)

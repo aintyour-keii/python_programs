0f40c09f619b84d62ba8118cb1e421951ddaf1b2
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-08 21:40:22 +0800

    pseudo code

---
+++ print_all_numbers_from_0_to_100_except_numbers_ending_in_0_or_5.py
@@ -0,0 +1,5 @@
+# Using a for loop, iterate to 0 - 100 (inclusive)
+# Convert the number to string
+# Get the last character of the string
+# Check if the last character of the string is not "0" or "5"
+# If the last character of the string is not "0" or "5", print the number

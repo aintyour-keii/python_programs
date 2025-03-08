27c2a90c945c4ec1fc862d9849d5e05f25e390a4
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-08 18:14:48 +0800

    code completion

--- print_all_numbers_from_0_to_100_except_numbers_ending_in_0.py
+++ print_all_numbers_from_0_to_100_except_numbers_ending_in_0.py
@@ -1,5 +1,13 @@
 # using a for loop, iterate from 0 - 100
-# convert the number to string
-# check the last digit of the string using string slicing
-# convert the last digit to int and check if it is 0
-# if it is 0 continue the loop otherwise print the number
+for i in range(101):
+    # convert the number to string
+    str_i = str(i)
+    # check the last digit of the string using string slicing
+    last_digit_string = str_i[-1]
+    # convert the last digit to int and check if it is 0
+    last_digit_int = int(last_digit_string)
+    # if it is 0 continue the loop otherwise print the number
+    if last_digit_int == 0:
+        continue
+    else:
+        print(i)

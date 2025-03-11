d9b47f5f2ad1269fffaaf808bfd7730701f13bb4
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-11 15:10:13 +0800

    pseudo code

---
+++ print_numbers_in_ascending_order.py
@@ -0,0 +1,8 @@
+# Initialize an empty list to store all inputted numbers
+# Continuously ask for a number input using a while True loop
+# Check if the input is valid using try/except
+# If the input is valid append the input to the list
+# Otherwise break out of the loop
+# Check if there were any valid inputs by checking on the list
+# If there were valid inputs, using the sort method, sort the list in ascending order
+# Print the sorted list

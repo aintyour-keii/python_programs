925f89a262a287a392196d343727a1cae4f4d4f7
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-12 00:30:26 +0800

    adding valid inputs to the list and sorting the list in reverse

--- print_all_inputted_numbers_in_descending_order.py
+++ print_all_inputted_numbers_in_descending_order.py
@@ -5,9 +5,11 @@ while True:
     # Inside the loop, ask the user to input a number with a try/except block to handle invalid inputs
     try: # If the input is valid, append the input to the list
         num = int(input("Enter a number: "))
-
+        numbers.append(num)
     except ValueError: # Otherwise, break out of the while loop
         break
 
 # Sort the list using the sort() function with the reverse=True argument to sort in descending order
+numbers.sort(reverse=True)
+
 # Print the sorted list

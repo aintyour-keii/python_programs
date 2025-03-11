8f9f42db0c227781b89e0526095f8f42b75cdac3
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-11 14:59:55 +0800

    pseudo code

---
+++ print_smallest_number_from_all_numbers.py
@@ -0,0 +1,8 @@
+# Initialize a variable to None, this will store the lowest number value
+# Continously ask for number input using a while True loop
+# Check if the number input is valid
+# If the input is a valid
+# Check if the variable is None or the input is less than the variable
+# If so, update the variable's value to the input
+# If the input is invalid, break out of the loop
+# Check if the variable is non "None", if so print the variable's value

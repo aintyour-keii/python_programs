85978d1d84bca8061644850e684a9f5895648665
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-12 00:15:32 +0800

    highest number storing and continuous number input unless invalid

--- print_highest_number.py
+++ print_highest_number.py
@@ -1,7 +1,13 @@
 # Initialize a variable that stores the highest number
+highest_number = None
 # Create an infinite loop using a while True statement
+while True:
 # Inside the loop, use a try/except block to handle potential errors
 # Try asking the user for a number, convert it to an integer
-# If the user enters an inter value, if the variable is not empty or if the inputted number is higher than the current highest number, update the variable
-# If the user enters a non-integer value, catch the ValueError and break out of the while loop
+    try: # If the user enters an inter value, if the variable is not empty or if the inputted number is higher than the current highest number, update the variable
+        num = int(input("Enter a number: "))
+        
+    except ValueError: # If the user enters a non-integer value, catch the ValueError and break out of the while loop
+        break
+    
 # Print the variable that store the highest number

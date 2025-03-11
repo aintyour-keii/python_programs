3a988acc5fec5d2a6d9298226cbea5c639ea2cfe
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-12 00:29:27 +0800

    input numbers tracking and continuous input until invalid

--- print_all_inputted_numbers_in_descending_order.py
+++ print_all_inputted_numbers_in_descending_order.py
@@ -1,7 +1,13 @@
 # Initialize a list to store all inputted numbers
+numbers = []
 # Create an infinite loop using a while True statement
-# Inside the loop, ask the user to input a number with a try/except block to handle invalid inputs
-# If the input is valid, append the input to the list
-# Otherwise, break out of the while loop
+while True:
+    # Inside the loop, ask the user to input a number with a try/except block to handle invalid inputs
+    try: # If the input is valid, append the input to the list
+        num = int(input("Enter a number: "))
+
+    except ValueError: # Otherwise, break out of the while loop
+        break
+
 # Sort the list using the sort() function with the reverse=True argument to sort in descending order
 # Print the sorted list

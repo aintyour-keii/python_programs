27ca1707f13ab8d05b04a19a40eef483804f45ec
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-11 14:48:43 +0800

    pseudo code

---
+++ print_if_number_is_unique_or_duplicate.py
@@ -0,0 +1,8 @@
+# Initialize an empty set to keep track of unique numbers
+# Using a while True loop
+# -> try asking for a valid number input
+# -> if the input is valid
+#   -> check if the number is the set
+#   -> if the number is found in the set print "Duplicate"
+#   -> otherwise, print "Unique" and add it to the set
+# -> if the input is invalid, break out of the loop and stop the program

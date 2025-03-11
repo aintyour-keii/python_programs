8a1fd87e0c06b7fc25b7a531cfca2de828f6cdf5
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-11 23:38:41 +0800

    inputted numbers tracking and number inputs

--- priny_all_duplicate_number_inputs.py
+++ priny_all_duplicate_number_inputs.py
@@ -1,8 +1,14 @@
 # Initialize a dictionary to store all the number inputs
+numbers = {}
 # Loop 10 times using a for loop
-# -> Ask for a number input
-# -> If the number exists in the dictionary, increment its value by 1
-# -> Otherwise, add the number in the dictionary with a count of 1
+for _ in range(10):
+    # -> Ask for a number input
+    num = input(input("Enter a number: "))
+    if num in numbers: # -> If the number exists in the dictionary, increment its value by 1
+        numbers[num] += 1
+    else: # -> Otherwise, add the number in the dictionary with a count of 1
+        numbers[num] = 1
+
 # Initialize a list to store all the duplicated numbers
 # Loop through the dictionary and add the number with a value of greater than 1 to the list
 # Print the list that stores all the numbers that was inputted more than once

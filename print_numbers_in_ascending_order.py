8b23b1f7c766dfc0d18a45f1e828ab1098d7d0a5
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-11 15:13:10 +0800

    numbers storage list and continuous number input unless invalid

--- print_numbers_in_ascending_order.py
+++ print_numbers_in_ascending_order.py
@@ -1,8 +1,16 @@
 # Initialize an empty list to store all inputted numbers
+numbers = []
+
 # Continuously ask for a number input using a while True loop
-# Check if the input is valid using try/except
-# If the input is valid append the input to the list
-# Otherwise break out of the loop
+while True:
+    # Check if the input is valid using try/except
+    try:
+        # If the input is valid append the input to the list
+        num = int(input("Enter a number: "))
+    except ValueError:
+        # Otherwise break out of the loop
+        break
+
 # Check if there were any valid inputs by checking on the list
 # If there were valid inputs, using the sort method, sort the list in ascending order
 # Print the sorted list

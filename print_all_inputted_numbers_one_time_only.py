586886d2b02c5ed1d0dd3a7ef84366ec2cad9e31
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-11 14:36:46 +0800

    pseudo code

---
+++ print_all_inputted_numbers_one_time_only.py
@@ -0,0 +1,8 @@
+# Initialize an empty list to store all numbers to be displayed
+# Initialize an empty set to store all seen inputs
+# Loop through 10 times:
+# -> Ask the user for a number input
+# -> Check if the input has yet to be seen
+# -> Append the input to the list
+# -> Add the input to the set
+# Print the list

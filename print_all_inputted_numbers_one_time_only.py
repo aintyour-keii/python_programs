460b678cd4499a8ec0118df25a6314b7ef1102aa
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-11 14:37:55 +0800

    list and set initialized

--- print_all_inputted_numbers_one_time_only.py
+++ print_all_inputted_numbers_one_time_only.py
@@ -1,5 +1,7 @@
 # Initialize an empty list to store all numbers to be displayed
+numbers_to_be_displayed = []
 # Initialize an empty set to store all seen inputs
+seen_inputs = set()
 # Loop through 10 times:
 # -> Ask the user for a number input
 # -> Check if the input has yet to be seen

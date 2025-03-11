9c5d4296da38c0ba806a97ee2b94dcc2c6117ea8
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-12 00:40:21 +0800

    input tracking and continuous input until invalid: valid input is added to the list

--- print_average_of_numbers.py
+++ print_average_of_numbers.py
@@ -1,7 +1,15 @@
 # Initialize a list to store all inputted numbers
+numbers = []
 # Create an infinite loop using a while True statement
-# Inside the loop, ask the user for a number with a try/except block to handle invalid inputs
-# If the input is valid then add the number to the list
-# Otherwise, break out of the while loop
+while True:
+    # Inside the loop, ask the user for a number with a try/except block to handle invalid inputs
+    # If the input is valid then add the number to the list
+    try: 
+        num = int(input("Enter a number: "))
+        numbers.append(num)
+    # Otherwise, break out of the while loop
+    except ValueError:
+        break
+
 # Calculate the average by using the sum() method to find the sum of all the numbers and len() to find the number of elements in the list
 # Print the average

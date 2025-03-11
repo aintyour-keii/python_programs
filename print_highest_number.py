7077b7c18ac60e45a53d1631bd94bfdd6608beda
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-12 00:13:39 +0800

    pseudo code

---
+++ print_highest_number.py
@@ -0,0 +1,7 @@
+# Initialize a variable that stores the highest number
+# Create an infinite loop using a while True statement
+# Inside the loop, use a try/except block to handle potential errors
+# Try asking the user for a number, convert it to an integer
+# If the user enters an inter value, if the variable is not empty or if the inputted number is higher than the current highest number, update the variable
+# If the user enters a non-integer value, catch the ValueError and break out of the while loop
+# Print the variable that store the highest number

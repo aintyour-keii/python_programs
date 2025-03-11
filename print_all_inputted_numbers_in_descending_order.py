022296bb7bb51e47ee5c53773de3fef8b0621253
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-12 00:27:38 +0800

    pseudo code

---
+++ print_all_inputted_numbers_in_descending_order.py
@@ -0,0 +1,7 @@
+# Initialize a list to store all inputted numbers
+# Create an infinite loop using a while True statement
+# Inside the loop, ask the user to input a number with a try/except block to handle invalid inputs
+# If the input is valid, append the input to the list
+# Otherwise, break out of the while loop
+# Sort the list using the sort() function with the reverse=True argument to sort in descending order
+# Print the sorted list

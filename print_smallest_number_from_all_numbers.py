a63da55d87be1136afe433f7be93fa18b8e8d3b8
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-11 15:02:23 +0800

    variable storage for lowest number and continuous number input unless invalid

--- print_smallest_number_from_all_numbers.py
+++ print_smallest_number_from_all_numbers.py
@@ -1,8 +1,16 @@
 # Initialize a variable to None, this will store the lowest number value
+lowest_number = None
 # Continously ask for number input using a while True loop
-# Check if the number input is valid
-# If the input is a valid
-# Check if the variable is None or the input is less than the variable
-# If so, update the variable's value to the input
-# If the input is invalid, break out of the loop
+while True:
+    # Check if the number input is valid
+    try:
+        num = int(input("Enter a number: "))
+        # If the input is a valid
+        # Check if the variable is None or the input is less than the variable
+        # If so, update the variable's value to the input
+        
+    # If the input is invalid, break out of the loop
+    except ValueError:
+        break
+
 # Check if the variable is non "None", if so print the variable's value

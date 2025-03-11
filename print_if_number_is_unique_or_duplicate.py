819767bee20e6ad35e66d36fd96b15cd23a63764
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-11 14:52:35 +0800

    identifying unique and duplicate number inputs

--- print_if_number_is_unique_or_duplicate.py
+++ print_if_number_is_unique_or_duplicate.py
@@ -7,8 +7,14 @@ while True:
         num = int(input("Enter a number: "))
         # -> if the input is valid
         #   -> check if the number is the set
-        #   -> if the number is found in the set print "Duplicate"
-        #   -> otherwise, print "Unique" and add it to the set
+        if num in unique_numbers:
+            #   -> if the number is found in the set print "Duplicate"
+            print("Duplicate")
+        else:
+            #   -> otherwise, print "Unique" and add it to the set
+            print("Unique")
+            unique_numbers.add(num)
+            
     except ValueError:
         # -> if the input is invalid, break out of the loop and stop the program
         break

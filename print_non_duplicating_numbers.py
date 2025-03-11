4f79a711775035e74860ab5274ad17e79459373e
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-11 14:10:25 +0800

    pseudo code

---
+++ print_non_duplicating_numbers.py
@@ -0,0 +1,8 @@
+# Initialize a dictionary to store all the number inputs
+# Loop 10 times using a for loop
+# -> Ask for a number input
+# -> If the number exists in the dictionary, increment its value by 1
+# -> Otherwise, add the number in the dictionary with a count of 1
+# Initialize a list to store all the unique numbers
+# Loop through the dictionary and add the number with a value of 1 to the list
+# Print the list that stores all the unique numbers

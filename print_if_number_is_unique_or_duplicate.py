9a43c79d66d96b152bbc5eb44382ac631041a91e
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-11 14:51:25 +0800

    unique numbers set and continous loop for number input until invalid input

--- print_if_number_is_unique_or_duplicate.py
+++ print_if_number_is_unique_or_duplicate.py
@@ -1,8 +1,14 @@
 # Initialize an empty set to keep track of unique numbers
+unique_numbers = set()
 # Using a while True loop
-# -> try asking for a valid number input
-# -> if the input is valid
-#   -> check if the number is the set
-#   -> if the number is found in the set print "Duplicate"
-#   -> otherwise, print "Unique" and add it to the set
-# -> if the input is invalid, break out of the loop and stop the program
+while True:
+    # -> try asking for a valid number input
+    try:
+        num = int(input("Enter a number: "))
+        # -> if the input is valid
+        #   -> check if the number is the set
+        #   -> if the number is found in the set print "Duplicate"
+        #   -> otherwise, print "Unique" and add it to the set
+    except ValueError:
+        # -> if the input is invalid, break out of the loop and stop the program
+        break

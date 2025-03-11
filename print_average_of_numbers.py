70553600a2de499dd5cae8d02b83cf34c4156fe4
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-12 00:37:56 +0800

    pseudo code

---
+++ print_average_of_numbers.py
@@ -0,0 +1,7 @@
+# Initialize a list to store all inputted numbers
+# Create an infinite loop using a while True statement
+# Inside the loop, ask the user for a number with a try/except block to handle invalid inputs
+# If the input is valid then add the number to the list
+# Otherwise, break out of the while loop
+# Calculate the average by using the sum() method to find the sum of all the numbers and len() to find the number of elements in the list
+# Print the average

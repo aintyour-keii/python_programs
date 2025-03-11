4cc4edb29d18586aede6086d48bef5e57e8714c4
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-11 23:54:17 +0800

    pseudo code

---
+++ print_number_with_most_duplicates.py
@@ -0,0 +1,11 @@
+# Initiate a dictionary to store all inputted numbers
+# Create an infinite loop using a while True loop
+# Inside the loop:
+# Using try/except block to catch any potential errors
+# Ask for a number input
+# If it is a valid input, check if it is in the dictionary and add one to its value 
+# otherwise add it to the dictionary with a value of 1
+# If the input is invalid break out of the loop
+# Initiate a variable to store the input with the most number of repetition
+# Loop throught the dictionary and identify the input with the most number of repetition
+# Print the variable that stores the input with the most number of repetition

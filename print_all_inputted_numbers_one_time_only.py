24493e602df424ce1fc6619318f470a1eba10bba
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-11 14:39:54 +0800

    number input and tracking for numbers to be displayed

--- print_all_inputted_numbers_one_time_only.py
+++ print_all_inputted_numbers_one_time_only.py
@@ -2,9 +2,16 @@
 numbers_to_be_displayed = []
 # Initialize an empty set to store all seen inputs
 seen_inputs = set()
+
 # Loop through 10 times:
-# -> Ask the user for a number input
-# -> Check if the input has yet to be seen
-# -> Append the input to the list
-# -> Add the input to the set
+for _ in range(10):
+    # -> Ask the user for a number input
+    num = int(input("Enter a number: "))
+    # -> Check if the input has yet to be seen
+    if num not in seen_inputs:
+    # -> Append the input to the list
+        numbers_to_be_displayed.append(num)
+    # -> Add the input to the set
+        seen_inputs.add(num)
+
 # Print the list

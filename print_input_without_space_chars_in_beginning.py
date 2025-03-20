af990d3b39618b14b07c0414134ed989e1cf6ea2
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-20 16:10:05 +0800

    pseudo code

---
+++ print_input_without_space_chars_in_beginning.py
@@ -0,0 +1,5 @@
+# Ask for the user to input their full name
+# Store the inputted full name in a variable
+# Modify the variable using the 'lstrip()' method of a string
+# Set the parameters of the method to just an empty space, inside quotations
+# Print the variable

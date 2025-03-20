2c8f2461000a1a76becd705739216a37e6ca0fdd
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-20 16:10:34 +0800

    full name input added

--- print_input_without_space_chars_in_beginning.py
+++ print_input_without_space_chars_in_beginning.py
@@ -1,5 +1,6 @@
 # Ask for the user to input their full name
 # Store the inputted full name in a variable
+full_name = input("Enter your full name: ")
 # Modify the variable using the 'lstrip()' method of a string
 # Set the parameters of the method to just an empty space, inside quotations
 # Print the variable
